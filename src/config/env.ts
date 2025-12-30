075b5374ac1cd10b483a3ad51283e1c45df1be5e
elmoustafi-22 <mustophaqadir@gmail.com>
2025-12-30 07:37:16 +0100

    feat: add environment variable configuration and validation

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,25 +1,21 @@
 import { config } from "dotenv";
-import path from "path";
 
-config({ path: path.join(__dirname, "../../.env") });
+config();
 
 export const env = {
   NODE_ENV: process.env.NODE_ENV || "development",
   PORT: parseInt(process.env.PORT || "5000", 10),
 
-  // Database
   MONGODB_URI: process.env.MONGODB_URI,
 
-  //Security
   JWT_SECRET: process.env.JWT_SECRET || "super_secret_fallback_change_this",
   JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN || "1d",
 
-  //Bcrypt Salt Rounds
-  SALT_ROUNDS: parseInt(process.env.SALT_ROUND || "10", 10),
+  SALT_ROUNDS: parseInt(process.env.SALT_ROUNDS || "10", 10),
 };
 
 if (!env.MONGODB_URI) {
-  throw new Error("❌ Fatal Error: MONGODB_URI is not defined in .env file");
+  throw new Error("❌ Fatal Error: MONGODB_URI is not defined");
 }
 
 if (
